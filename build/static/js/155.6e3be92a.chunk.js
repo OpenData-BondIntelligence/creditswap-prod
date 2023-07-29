ba0fbcd937a16a2c1b8f52396e30a8b71969fb81
Eesha Jain <68827992+Eesha-Jain@users.noreply.github.com>
2023-07-28 18:46:44 -0700

    update url

--- build/static/js/155.6e3be92a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunk_uniswap_interface=self.webpackChunk_uniswap_interface||[]).push([[155],{98155:function(e,n,s){s.r(n),s.d(n,{default:function(){return u}});var u={messages:{}}}}]);
-//# sourceMappingURL=155.6e3be92a.chunk.js.map
